a812217f53e4071dd0c06affcf4ccb7c7255d3eb
termanico <klingbeil.thorsten@t-online.de>
2021-12-27 22:06:01 +0100

    u

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,8 +2,12 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/m/MessageBox",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, MessageBox, JSONModel) {
+], function (Controller, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -12,13 +16,43 @@ sap.ui.define([
 		 *  Hook for initializing the controller
 		 */
 		onInit: function () {
-			var oJSONData = {
-				busy: false
-			};
-			var oModel = new JSONModel(oJSONData);
-			this.getView().setModel(oModel, "appView");
-		},
+			var oMessageManager = sap.ui.getCore().getMessageManager(),
+				oMessageModel = oMessageManager.getMessageModel(),
+				oMessageModelBinding = oMessageModel.bindList("/", undefined, [],
+					new Filter("technical", FilterOperator.EQ, true)),
+				oViewModel = new JSONModel({
+					busy: false,
+					hasUIChanges: false,
+					usernameEmpty: true,
+					order: 0
+				});
+			this.getView().setModel(oViewModel, "appView");
+			this.getView().setModel(oMessageModel, "message");
 
+			oMessageModelBinding.attachChange(this.onMessageBindingChange, this);
+			this._bTechnicalErrors = false;
+		},
+		onCreate: function () {
+			var oList = this.byId("peopleList"),
+				oBinding = oList.getBinding("items"),
+				oContext = oBinding.create({
+					"UserName": "",
+					"FirstName": "",
+					"LastName": "",
+					"Age": "18"
+				});
+
+			this._setUIChanges();
+			this.getView().getModel("appView").setProperty("/usernameEmpty", true);
+
+			oList.getItems().some(function (oItem) {
+				if (oItem.getBindingContext() === oContext) {
+					oItem.focus();
+					oItem.setSelected(true);
+					return true;
+				}
+			});
+		},
 		onRefresh: function () {
 			var oBinding = this.byId("peopleList").getBinding("items");
 
@@ -29,10 +63,89 @@ sap.ui.define([
 			oBinding.refresh();
 			MessageToast.show(this._getText("refreshSuccessMessage"));
 		},
+		onSave: function () {
+			var fnSuccess = function () {
+				this._setBusy(false);
+				MessageToast.show(this._getText("changesSentMessage"));
+				this._setUIChanges(false);
+			}.bind(this);
 
+			var fnError = function (oError) {
+				this._setBusy(false);
+				this._setUIChanges(false);
+				MessageBox.error(oError.message);
+			}.bind(this);
+
+			this._setBusy(true); // Lock UI until submitBatch is resolved.
+			this.getView().getModel().submitBatch("peopleGroup").then(fnSuccess, fnError);
+			this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
+		},
+		onSearch: function () {
+			var oView = this.getView(),
+				sValue = oView.byId("searchField").getValue(),
+				oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+
+			oView.byId("peopleList").getBinding("items").filter(oFilter, FilterType.Application);
+		},
+		onSort: function () {
+			var oView = this.getView(),
+				aStates = [undefined, "asc", "desc"],
+				aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+				sMessage,
+				iOrder = oView.getModel("appView").getProperty("/order");
+
+			iOrder = (iOrder + 1) % aStates.length;
+			var sOrder = aStates[iOrder];
+
+			oView.getModel("appView").setProperty("/order", iOrder);
+			oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+
+			sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+			MessageToast.show(sMessage);
+		},
+		onMessageBindingChange: function (oEvent) {
+			var aContexts = oEvent.getSource().getContexts(),
+				aMessages,
+				bMessageOpen = false;
+
+			if (bMessageOpen || !aContexts.length) {
+				return;
+			}
+
+			// Extract and remove the technical messages
+			aMessages = aContexts.map(function (oContext) {
+				return oContext.getObject();
+			});
+			sap.ui.getCore().getMessageManager().removeMessages(aMessages);
+
+			this._setUIChanges(true);
+			this._bTechnicalErrors = true;
+			MessageBox.error(aMessages[0].message, {
+				id: "serviceErrorMessageBox",
+				onClose: function () {
+					bMessageOpen = false;
+				}
+			});
+
+			bMessageOpen = true;
+		},
 		_getText: function (sTextId, aArgs) {
 			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
 
+		},
+		_setUIChanges: function (bHasUIChanges) {
+			if (this._bTechnicalErrors) {
+				// If there is currently a technical error, then force 'true'.
+				bHasUIChanges = true;
+			} else if (bHasUIChanges === undefined) {
+				bHasUIChanges = this.getView().getModel().hasPendingChanges();
+			}
+			var oModel = this.getView().getModel("appView");
+			oModel.setProperty("/hasUIChanges", bHasUIChanges);
+		},
+		_setBusy: function (bIsBusy) {
+			var oModel = this.getView().getModel("appView");
+			oModel.setProperty("/busy", bIsBusy);
 		}
 	});
 });
