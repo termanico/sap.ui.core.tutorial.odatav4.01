3435d2f0243afbb9aa13c69fb15d3c0ca1132c7b
termanico <klingbeil.thorsten@t-online.de>
2021-12-27 20:04:59 +0100

    u

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+], function (Controller, MessageToast, MessageBox, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -9,12 +11,28 @@ sap.ui.define([
 		/**
 		 *  Hook for initializing the controller
 		 */
-		onInit : function () {
+		onInit: function () {
 			var oJSONData = {
-				busy : false
+				busy: false
 			};
 			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
+		},
+
+		onRefresh: function () {
+			var oBinding = this.byId("peopleList").getBinding("items");
+
+			if (oBinding.hasPendingChanges()) {
+				MessageBox.error(this._getText("refreshNotPossibleMessage"));
+				return;
+			}
+			oBinding.refresh();
+			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+
+		_getText: function (sTextId, aArgs) {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
+
 		}
 	});
 });
